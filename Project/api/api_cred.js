cbfcabcc73b708a8cac693b78a5cf3d4b213c1c4
Pedro Vieira <61158116+iampedrovieira@users.noreply.github.com>
2021-02-15 12:55:56 +0000

    Update api_cred.js

--- Project/api/api_cred.js
+++ Project/api/api_cred.js
@@ -1,6 +1,6 @@
 const credentials = {
-    api_football_data_token: '96a8ef21d3894e3a828033c76178d395',
-    x_api_rapid: '516faa150dmsh0b8085f9554b32ap105bc8jsn49f3f69ef605',
-    x_api_rapid_2: 'aaa1bb79ecmsh383ca039349ca73p1ec0fajsnbe36458e8a28',
-    news_api: 'be867192d60d473085b7134b3be1a70d'
+    api_football_data_token: '-',
+    x_api_rapid: '-',
+    x_api_rapid_2: '-',
+    news_api: '-'
 }
